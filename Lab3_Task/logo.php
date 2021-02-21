01604d2123c9037f5df404c142b88f13a70ffbb9
Abir-K <abirbeatz@gmail.com>
2021-02-21 21:12:57 +0600

    labtask4

--- Lab3_Task/logo.php
+++ Lab3_Task/logo.php
@@ -1,11 +1,11 @@
 <?php
 
-$backColor="yellow"; $fontColor="cyan"; $imageWidth="80px"; $imageHeight="80px";
+$backColor="green"; $fontColor="red"; $imageWidth="80px"; $imageHeight="80px";
 
 ?>
 <img src="aiub_logo.jpg" width="300px" height="300px" />
 
-<h2>American International University -<span style="background-color:$backColor;color:$fontColor">
+<h2>American International University -<span style="background-color:<?php echo $backColor?>;color:<?php echo $fontColor?>">
 Bangladesh</h2>
 
 </span>
